3bd88fe248f3d4bce2b4b8715b5334d48bf44d6b
Tamas Koczka <poprdi@google.com>
2024-12-13 12:07:08 +0000

    expkit: test: add option to list test (and ArgumentParser class)
    Change-Id: Iafd2ebfa0ebbfeb9f119abb3801cb59206854166

--- expkit/test/main.cpp
+++ expkit/test/main.cpp
@@ -4,17 +4,37 @@
 #include <iostream>
 #include "test/TestRunner.cpp"
 #include "test/tests/TargetDbTests.cpp"
+#include "util/ArgumentParser.cpp"
 
 class Main {
+    ArgumentParser args_;
     TestRunner runner_;
 
+    void ListTests() {
+        for (auto& testSuite : runner_.GetTestSuites()) {
+            cout << testSuite->class_name << ": ";
+            for (int i = 0; i < testSuite->tests.size(); i++)
+                cout << (i == 0 ? "" : ", ") << testSuite->tests[i].func_name;
+            cout << "\n";
+        }
+    }
 public:
-    Main(int argc, const char* argv[]) {
+    Main(int argc, const char* argv[]): args_(argc, argv) {
         runner_.Add(new TargetDbTests());
     }
 
     int Run() {
-        return runner_.Run() ? 0 : 1;
+        auto& posArgs = args_.getPositionalArgs();
+        if (posArgs.size() >= 1 && posArgs[0] == "list") {
+            ListTests();
+        } else if (posArgs.size() == 0 || posArgs[0] == "run") {
+            return runner_.Run() ? 0 : 1;
+        } else {
+            cerr << "Unknown command." << endl;
+            return 1;
+        }
+
+        return 0;
     }
 };
 
