2e198118d99d32cb700db563897f0c3d609f448f
Tamas Koczka <poprdi@google.com>
2024-12-15 14:14:07 +0000

    Fix register order (RBP comes before RSP), move Register to separate file (to be for other purposes)
    Change-Id: I08e6a9274038050e7854ccc76a6e82b383c845d0

--- expkit/pivot/Pivot.cpp
+++ expkit/pivot/Pivot.cpp
@@ -2,8 +2,8 @@
 
 #include <cstdint>
 #include <vector>
+#include "util/Register.cpp"
 
-enum class Register { RAX, RBX, RCX, RDX, RSI, RDI, RSP, RBP, R8, R9, R10, R11, R12, R13, R14, R15 };
 enum class IndirectType { JMP, CALL };
 
 struct Pivot {
