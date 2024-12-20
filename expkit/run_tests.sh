326c02e46bd42393a857880dd94cb837992eea8b
Tamas Koczka <poprdi@google.com>
2024-12-20 16:07:39 +0000

    expkit: test: fix relative path and auto-create bin folder

--- expkit/run_tests.sh
+++ expkit/run_tests.sh
@@ -1,7 +1,7 @@
 set -ex
 make test
-cp bin/test ../../tools/kernel-image-runner/rootfs/test_runner
-mkdir -p ../../tools/kernel-image-runner/rootfs/test/
-cp -r test/artifacts ../../tools/kernel-image-runner/rootfs/test/
-../../tools/kernel-image-runner/run.sh kernelctf lts-6.1.81 --custom-modules=kpwn --only-command-output --dmesg=dmesg.txt -- /test_runner --tap --target-db test/artifacts/target_db_lts-6.1.81.kpwn
+cp bin/test ../kernel-image-runner/rootfs/test_runner
+mkdir -p ../kernel-image-runner/rootfs/test/
+cp -r test/artifacts ../kernel-image-runner/rootfs/test/
+../kernel-image-runner/run.sh kernelctf lts-6.1.81 --custom-modules=kpwn --only-command-output --dmesg=dmesg.txt -- /test_runner --tap --target-db test/artifacts/target_db_lts-6.1.81.kpwn
 test/check_test_run.py
