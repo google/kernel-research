e512e6953f0bcf8f23bf87022a3d9c8d820ed1e4
Tamas Koczka <poprdi@google.com>
2025-02-06 09:38:38 +0000

    GHA: add daily test runner for multi-target tests

--- expkit/run_tests.sh
+++ expkit/run_tests.sh
@@ -1,7 +1,14 @@
 set -ex
+
+DISTRO="${1:-kernelctf}"
+RELEASE_NAME="${2:-lts-6.1.81}"
+TEST_RUNNER_ARGS="${@:3}"
+
 make test
 cp bin/test ../kernel-image-runner/rootfs/test_runner
+
 mkdir -p ../kernel-image-runner/rootfs/test/
 cp -r test/artifacts ../kernel-image-runner/rootfs/test/
-../kernel-image-runner/run.sh kernelctf lts-6.1.81 --custom-modules=kpwn --only-command-output --dmesg=dmesg.txt -- /test_runner --tap --target-db test/artifacts/target_db_lts-6.1.81.kpwn $@ | tee tap_results.txt
+
+../kernel-image-runner/run.sh "$DISTRO" "$RELEASE_NAME" --custom-modules=kpwn --only-command-output --dmesg=dmesg.txt -- /test_runner --tap --target-db test/artifacts/kernelctf.kpwn $TEST_RUNNER_ARGS | tee tap_results.txt
 test/check_test_run.py
