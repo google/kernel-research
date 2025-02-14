b43a643aff00986a27088c05bd7c1f1749a6707d
Tamas Koczka <poprdi@google.com>
2025-02-14 13:26:18 +0000

    GHA: update-db: merge db at the end

---
+++ scripts/db_merge.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -ex
+
+SCRIPT_DIR=$(dirname $(realpath "$0"))
+IMAGE_DB_DIR="$SCRIPT_DIR/../kernel-image-db"
+KPWN_DB_DIR="$SCRIPT_DIR/../kpwn_db"
+
+mkdir -p db
+gcloud storage cp "gs://kernel-research/pwnkit/db/kernelctf/*.kpwn" ./db
+"$KPWN_DB_DIR/kpwn_db.py" -i "db/*.kpwn" -o kernelctf.kpwn
+
+echo "Uploading new db"
+gcloud storage cp -Z -a publicRead kernelctf.kpwn gs://kernel-research/pwnkit/db/kernelctf.kpwn
