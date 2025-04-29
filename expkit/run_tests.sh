d3d4c7013286b57e9e7c0f6cb819a06271aaf3a6
Tamas Koczka <poprdi@google.com>
2025-04-29 14:21:44 +0000

    GHA: tests now run 20 rounds instead of 1

--- expkit/run_tests.sh
+++ expkit/run_tests.sh
@@ -1,3 +1,4 @@
+set -e
 
 DISTRO="${1:-kernelctf}"
 RELEASE_NAME="${2:-lts-6.1.81}"
@@ -13,7 +14,7 @@ cp -r test/artifacts ../kernel-image-runner/rootfs/test/
 ../kernel-image-runner/compile_custom_modules.sh "$DISTRO" "$RELEASE_NAME" kpwn
 
 mkdir -p test_results
-rm test_results/round_* test_results/dmesg_* 2>/dev/null
+rm test_results/round_* test_results/dmesg_* 2>/dev/null || true
 
 for i in $(seq 1 $TIMES); do
     ../kernel-image-runner/run.sh "$DISTRO" "$RELEASE_NAME" --custom-modules=keep --only-command-output --dmesg=test_results/dmesg_$i.txt -- /test_runner --target-db test/artifacts/kernelctf.kpwn $TEST_RUNNER_ARGS > test_results/round_$i.txt &
@@ -21,12 +22,15 @@ done
 
 wait
 
+printf "\nTAP results:\n"
+cat test_results/round_1.txt
+
+printf "\nSummary:\n"
 if [[ "$TIMES" == "1" ]]; then
-    cat test_results/round_1.txt
     test/check_test_run.py test_results/round_1.txt
 else
     for i in $(seq 1 $TIMES); do
         echo -n "Round #$i: "
-        test/check_test_run.py test_results/round_$i.txt && echo "ok"
+        test/check_test_run.py test_results/round_$i.txt && echo "ok" || cat test_results/round_$i.txt
     done
 fi
