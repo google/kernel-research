15af197c9b412a76960f63dc07d6497804120c54
Tamas Koczka <poprdi@google.com>
2025-05-08 10:15:17 +0000

    expkit: test: refactor RopActionTests to always test stack usage

--- expkit/test/tests/RopActionTests.cpp
+++ expkit/test/tests/RopActionTests.cpp
@@ -6,6 +6,7 @@
 #include "test/TestUtils.cpp"
 #include "util/HexDump.cpp"
 #include "util/error.cpp"
+#include "payloads/RopChain.cpp"
 #include "payloads/Payload.cpp"
 
 class RopActionTests: public TestSuite {
@@ -17,25 +18,57 @@ public:
         kpwn_ = &env->GetKpwn();
     }
 
-    TEST_METHOD(writeWhatWhereTest, "WRITE_WHAT_WHERE_64 is working") {
-        auto target = env->GetTarget();
-        auto kaslr_base = kpwn_->KaslrLeak();
-        auto rip_recovery = kpwn_->GetRipControlRecoveryAddr();
+    RopChain GetRopChain() {
+        return RopChain(env->GetTarget(), kpwn_->KaslrLeak());
+    }
 
+    // return true in case of a fork
+    bool ExecuteRopChain(RopChain& rop, uint64_t min_stack = 0, uint64_t max_stack = 4096, uint64_t buf_size = 4096) {
+        rop.Add(kpwn_->GetRipControlRecoveryAddr());
+
+        Payload p(buf_size);
+        auto rop_offs = p.Size() - rop.GetByteSize();
+        p.Set(rop_offs, rop.GetData());
+
+        auto rop_buf_addr = kpwn_->AllocBuffer(p.GetData(), true);
+        Log("rop_buf_addr = 0x%lx", rop_buf_addr);
+
+        auto orig_pid = getpid();
+        kpwn_->SetRspAndRet(rop_buf_addr + rop_offs);
+        if (getpid() != orig_pid)
+            return true;
+
+        auto used_stack = kpwn_->Read(rop_buf_addr, rop_offs);
+        Log("Stack content:\n%s", HexDump::Dump(used_stack).c_str());
+        kpwn_->Kfree(rop_buf_addr);
+
+        int non_used_bytes = 0;
+        for (; non_used_bytes < used_stack.size(); non_used_bytes++)
+            if (used_stack[non_used_bytes] != 0)
+                break;
+
+        auto used_stack_size = used_stack.size() - non_used_bytes;
+        if (used_stack_size < min_stack || used_stack_size > max_stack)
+            throw ExpKitError("Expected stack size between %u .. %u bytes, but it used %u bytes", min_stack, max_stack, used_stack_size);
+
+        return false;
+    }
+
+    // return true in case of a fork
+    bool ExecuteRopAction(RopActionId id, std::vector<uint64_t> arguments = {}, uint64_t min_stack = 0, uint64_t max_stack = 4096, uint64_t buf_size = 4096) {
+        auto rop = GetRopChain();
+        rop.AddRopAction(id, arguments);
+        return ExecuteRopChain(rop);
+    }
+
+
+    TEST_METHOD(writeWhatWhereTest, "WRITE_WHAT_WHERE_64 is working") {
+        Payload p(40);
+        auto target_offs = 16;
         uint64_t new_value = 0x1122334455667788;
 
-        Payload p(40);
-
-        auto target_offs = 16;
         auto target_buf_addr = kpwn_->AllocBuffer(p.GetData(), true);
-
-        RopChain rop(target, kaslr_base);
-        rop.AddRopAction(RopActionId::WRITE_WHAT_WHERE_64, {target_buf_addr + target_offs, new_value});
-        rop.Add(rip_recovery);
-
-        auto rop_buf_addr = kpwn_->AllocBuffer(rop.GetData(), true);
-        kpwn_->SetRspAndRet(rop_buf_addr);
-        kpwn_->Kfree(rop_buf_addr);
+        ExecuteRopAction(RopActionId::WRITE_WHAT_WHERE_64, {target_buf_addr + target_offs, new_value}, 0, 0);
 
         auto buf_leak = kpwn_->Read(target_buf_addr, p.Size());
         Log("Leaked buffer:\n%s", HexDump::Dump(buf_leak).c_str());
@@ -46,41 +79,11 @@ public:
     }
 
     TEST_METHOD(teleforkTest, "TELEFORK is working, its stack usage is in expected range") {
-        auto target = env->GetTarget();
-        auto kaslr_base = kpwn_->KaslrLeak();
-        auto rip_recovery = kpwn_->GetRipControlRecoveryAddr();
-        auto orig_pid = getpid();
-
-        RopChain rop(target, kaslr_base);
-        rop.AddRopAction(RopActionId::TELEFORK, {2000});
-        rop.Add(rip_recovery);
-
-        Payload p(4096);
-        auto rop_offs = p.Size() - rop.GetByteSize();
-        p.Set(rop_offs, rop.GetData());
-
-        auto buf_addr = kpwn_->AllocBuffer(p.GetData(), true);
-        Log("buf_addr = 0x%lx", buf_addr);
-
-        kpwn_->SetRspAndRet(buf_addr + rop_offs);
-        if (getpid() != orig_pid)
+        if (ExecuteRopAction(RopActionId::TELEFORK, {10}, 400, 2600))
             exit(123);
 
         int status;
         if (wait(&status) == -1 || !WIFEXITED(status) || WEXITSTATUS(status) != 123)
             throw ExpKitError("No child was forked.");
-
-        auto used_stack = kpwn_->Read(buf_addr, rop_offs);
-        Log("Stack content:\n%s", HexDump::Dump(used_stack).c_str());
-        kpwn_->Kfree(buf_addr);
-
-        int non_used_bytes = 0;
-        for (; non_used_bytes < used_stack.size(); non_used_bytes++)
-            if (used_stack[non_used_bytes] != 0)
-                break;
-
-        auto used_stack_size = used_stack.size() - non_used_bytes;
-        if (used_stack_size < 400 || used_stack_size > 2600)
-            throw ExpKitError("Expected telefork to use stack size between 400 .. 2600 bytes, but it used %u bytes", used_stack_size);
     }
 };
