4f25c477cdc349238cc39bbbdf65baa0484252be
Artem Metla <ametla@google.com>
2025-06-12 16:43:04 +0200

    expkit: add int is_kaslr_base(uint64_t kbase_addr) to utils. Use StaticTarget for structs and symbols in exp151.

--- expkit/util/pwn_utils.cpp
+++ expkit/util/pwn_utils.cpp
@@ -9,6 +9,12 @@ uint64_t check_kaslr_base(uint64_t kbase_addr) {
     return kbase_addr;
 }
 
+int is_kaslr_base(uint64_t kbase_addr) {
+    if ((kbase_addr & 0xFFFF0000000FFFFF) != 0xFFFF000000000000)
+        return 0;
+    return 1;
+}
+
 uint64_t check_heap_ptr(uint64_t heap_leak) {
     if ((heap_leak & 0xFFFF000000000000) != 0xFFFF000000000000)
         throw ExpKitError("kernel heap address (%p) is incorrect", heap_leak);
