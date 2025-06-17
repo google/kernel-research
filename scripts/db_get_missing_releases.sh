04792f2cd15510b2d18b585d026a821734738f58
Tamas Koczka <poprdi@google.com>
2025-06-17 15:44:04 +0000

    GHA: add missing DB release: do not reprocess targets if DB already exists

--- scripts/db_get_missing_releases.sh
+++ scripts/db_get_missing_releases.sh
@@ -20,18 +20,25 @@ KPWN_DB_DIR="$SCRIPT_DIR/../kpwn_db"
 
 if [ "$1" == "--rebuild" ]; then
     echo -n > db_releases.txt
+    echo -n > gcs_releases.txt
 else
     gcloud storage cp gs://kernel-research/pwnkit/db/kernelctf.kpwn db.kpwn; echo
     "$KPWN_DB_DIR/kpwn_db.py" -i db.kpwn --list-targets | grep kernelctf | sed "s/kernelctf\///" > db_releases.txt
+
+    gcloud storage ls gs://kernel-research/pwnkit/db/kernelctf/*.kpwn |sed -E "s/.*kernelctf\/(.*)\.kpwn/\\1/" > gcs_releases.txt
 fi
 
 # gcloud storage ls gs://kernelctf-build/releases | sed "s/.*releases\/\(.*\)\//\1/" > build_releases.txt
 
 # missing_releases = kernelctf_releases without db_releases and skipped_releases
 curl -s https://raw.githubusercontent.com/google/security-research/master/kernelctf/server/releases.yaml | grep -Eoh "^[^: ]*"  > kernelctf_releases.txt
-cat kernelctf_releases.txt | grep -v -f db_releases.txt | grep -v -f <(cat skipped_releases.txt | sed 's/\s*#.*//') > missing_releases.txt || true
-if [[ ! -s "missing_releases.txt" ]]; then echo "Nothing is missing from the database, exiting..."; exit 0; fi
+cat kernelctf_releases.txt | grep -v -f gcs_releases.txt | grep -v -f <(cat skipped_releases.txt | sed 's/\s*#.*//') > missing_gcs_releases.txt || true
+cat gcs_releases.txt | grep -v -f db_releases.txt > missing_db_releases.txt || true
 
-echo "The following releases missing from the DB, adding them now:"
-cat missing_releases.txt | sed 's/$/, /' | tr -d '\n'
+echo "The following releases were not processed yet: "
+cat missing_gcs_releases.txt | sed 's/$/, /' | tr -d '\n'
+echo
+
+echo "The following releases were not merged into the DB yet: "
+cat missing_db_releases.txt | sed 's/$/, /' | tr -d '\n'
 echo
