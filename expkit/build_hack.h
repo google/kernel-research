e0bce3328300bfc7587553ee0114e449b7017d66
Tamas Koczka <poprdi@google.com>
2025-06-19 14:28:01 +0000

    expkit: ugly hack to make tests work

---
+++ expkit/build_hack.h
@@ -0,0 +1,8 @@
+// ugly hack to include cpp files into compilation until we make a proper build system
+#include "payloads/Payload.cpp"
+#include "payloads/PayloadBuilder.cpp"
+#include "payloads/RopChain.cpp"
+#include "target/BinaryReader.cpp"
+#include "target/KpwnParser.cpp"
+#include "target/Target.cpp"
+#include "target/TargetDb.cpp"
