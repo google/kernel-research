7b82f71a24e0e70295c88d4e1e9800218eb78271
Tamas Koczka <poprdi@google.com>
2025-07-31 09:59:48 +0000

    expkit: postrip.h now includes RopUtils.h too

--- expkit/include/kernelXDK/postrip.h
+++ expkit/include/kernelXDK/postrip.h
@@ -1,4 +1,5 @@
 #include <kernelXDK/payloads/PayloadBuilder.h>
-#include <kernelXDK/pivot/StackPivot.h>
 #include <kernelXDK/payloads/RopChain.h>
 #include <kernelXDK/pivot/PivotFinder.h>
+#include <kernelXDK/pivot/StackPivot.h>
+#include <kernelXDK/rip/RopUtils.h>
